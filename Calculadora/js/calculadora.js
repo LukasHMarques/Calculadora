3489932bb7dc1c2295ced82443d1e95f45251c34
LukasHMarques <dev.lucashmarques@outlook.com>
2022-01-11 22:19:22 -0300

    Adicionando comentários
    Adicionei comentários onde precisa, e fiz um IF para que não deixe aparecer no visor dois operadores sem número EX: 1++-1 = 2

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,34 +1,66 @@
-function insert(num){
+/* OLHAR O retirar_operador, VC VAI USAR QUASE A MESMA LOGICA DO BOTÃO DE RETIRAR O ULTIMO CARACTRE "X", MAIS ESSE BOTÃO NÃO PODE CANCELAR O OPERADOR */
+
+function insert(num) { /* Adiciona os números no visor */
     document.principal.visor.value = document.principal.visor.value + num;
+    operador_ativado = false;
 }
 
-function apagarTudo() {
+function apagarTudo() { /*Apaga tudo sobre a conta */
     document.principal.visor.value = "";
+    operador_ativado = false;
 }
 
-function apagarNumero() {
+function apagarNumero() { /*Apaga o ultimo número inserido na conta antes de um operador */
     document.principal.visor.value = ""; /* ARRUMAR, APAGAR APENAS NÚMERO DO MOMENTO ESCRITO E NÃO A CONTA TODA */
 }
 
 
 /* Operadores */
-function bot_adicao(){
-    document.principal.visor.value = document.principal.visor.value + "+";
+function bot_adicao() {
+    if (operador_ativado == true) { /* Acaso for operador o ultimo caractere, irá substituir pela adição*/
+        visor = document.principal.visor.value;
+        retirar_operador = visor.substring(0, visor.length - 1);
+        document.principal.visor.value = retirar_operador + "+";
+    } else { /* Acaso não houver operador no ultimo caractere, irá adicionar o operador de adição*/
+        operador_ativado = true;
+        document.principal.visor.value = document.principal.visor.value + "+";
+    }
 }
 
-function bot_subtracao(){
-    document.principal.visor.value = document.principal.visor.value + "-";
+function bot_subtracao() {
+    if (operador_ativado == true) { /* Acaso for operador o ultimo caractere, irá substituir pela subtração*/
+        visor = document.principal.visor.value;
+        retirar_operador = visor.substring(0, visor.length - 1);
+        document.principal.visor.value = retirar_operador + "-";
+    } else { /* Acaso não houver operador no ultimo caractere, irá adicionar o operador de subtração*/
+        operador_ativado = true;
+        document.principal.visor.value = document.principal.visor.value + "-";
+    }
 }
 
-function bot_multiplicação(){
-    document.principal.visor.value = document.principal.visor.value + "x";
+function bot_multiplicação() {
+    if (operador_ativado == true) { /* Acaso for operador o ultimo caractere, irá substituir pela multiplicação*/
+        visor = document.principal.visor.value;
+        retirar_operador = visor.substring(0, visor.length - 1);
+        document.principal.visor.value = retirar_operador + "x";
+    } else { /* Acaso não houver operador no ultimo caractere, irá adicionar o operador de multiplicação*/
+        operador_ativado = true;
+        document.principal.visor.value = document.principal.visor.value + "x";
+    }
 }
 
-function bot_divisao(){
-    document.principal.visor.value = document.principal.visor.value + "÷";
+function bot_divisao() {
+    if (operador_ativado == true) { /* Acaso for operador o ultimo caractere, irá substituir pela divisão*/
+        visor = document.principal.visor.value;
+        retirar_operador = visor.substring(0, visor.length - 1);
+        document.principal.visor.value = retirar_operador + "÷";
+    } else { /* Acaso não houver operador no ultimo caractere, irá adicionar o operador de divisão*/
+        operador_ativado = true;
+        document.principal.visor.value = document.principal.visor.value + "÷";
+    }
 }
 /* Fim Operadores */
 
-function resultado(){
-    
+function resultado() {
+
 }
