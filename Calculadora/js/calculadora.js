205b3cbee911d21de7bfe897bf8c6f25655f99ec
LukasHMarques <dev.lucashmarques@outlook.com>
2022-01-15 14:27:11 -0300

    Separando os ONCLICKS dos números e operações

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,4 +1,4 @@
-function insert(num){
+function insert(num){ /* Inserir os números no visor */
     var numero = document.getElementById('visor').innerHTML;
     document.getElementById('visor').innerHTML = numero + num;
 }
@@ -11,6 +11,11 @@ function apagar_ultimo_caractere(){
     var resultado = document.getElementById('visor').innerHTML;
     document.getElementById('visor').innerHTML = resultado.substring(0, resultado.length -1);
 }
+
+function insert_operacao(operacao){ /* Inserir no visor as OPERAÇÕES */
+    var numero = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = numero + operacao;
+} 
 
 function calcular(){
     var resultado = document.getElementById('visor').innerHTML;
