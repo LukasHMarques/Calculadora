bd82c3ca01252d30959738966b2def9d43a2250a
LukasHMarques <dev.lucashmarques@outlook.com>
2022-01-17 11:53:26 -0300

    Adicionando a tela do visor da conta

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,24 +1,38 @@
 function insert(num){ /* Inserir os números no visor */
-    var numero = document.getElementById('visor').innerHTML;
+    numero = document.getElementById('visor').innerHTML;
     document.getElementById('visor').innerHTML = numero + num;
 }
 
 function apagarTudo(){
     document.getElementById('visor').innerHTML = "";
+    document.getElementById('visor_conta').innerHTML = "";
+}
+
+function apagarVisor(){
+    document.getElementById('visor').innerHTML = "";
 }
 
 function apagar_ultimo_caractere(){
-    var resultado = document.getElementById('visor').innerHTML;
+    resultado = document.getElementById('visor').innerHTML;
     document.getElementById('visor').innerHTML = resultado.substring(0, resultado.length -1);
 }
 
 function insert_operacao(operacao){ /* Inserir no visor as OPERAÇÕES */
-    var numero = document.getElementById('visor').innerHTML;
-    document.getElementById('visor').innerHTML = numero + operacao;
+    numero = document.getElementById('visor').innerHTML;
+    visor_conta = document.getElementById('visor_conta').value;
+    document.getElementById('visor_conta').innerHTML = visor_conta + numero + operacao;
+    document.getElementById('visor').innerHTML = "";
 } 
 
 function calcular(){
-    var resultado = document.getElementById('visor').innerHTML;
+    /* SE CLICAR NO RESULTADO E TIVER UM NUMERO NO VISOR PRINCIPAL, ADICIONAR ELE COM O RESTO DA CONTA DO VISOR_CONTA */
+    resultado = document.getElementById('visor_conta').innerHTML;
+    ultimo_caractere = resultado.slice(-1);
+
+    if (ultimo_caractere == "+" || ultimo_caractere == "-" || ultimo_caractere == "*" || ultimo_caractere == "/" ){
+        resultado = resultado.substring(0, resultado.length -1)
+    }
+
     if(resultado){
         document.getElementById('visor').innerHTML = eval(resultado);
     }
