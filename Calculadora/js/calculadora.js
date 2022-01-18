197b6941f662def3e5bdc95d013e318ab5fc25a7
LukasHMarques <dev.lucashmarques@outlook.com>
2022-01-18 10:13:12 -0300

    Funcionando Operações Básicas

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -25,21 +25,13 @@ function insert_operacao(operacao) { /* Inserir no visor as OPERAÇÕES */
     numero = document.getElementById('visor').value;
     visor_conta = document.getElementById('visor_conta').value;
 
-    document.getElementById('visor_conta').innerHTML = eval(visor_conta + numero) + operacao;
-    resultado_rapido = eval(visor_conta + numero) + operacao;
-    document.getElementById('visor').innerHTML = "";
-
-    /* Fazer com que a pessoa consiga mudar o operador depois de ter feito a linha a cima */
-
-
-    /* Capturar ultimo caractere para usar no IF abaixo
-    ultimo_caractere = visor_conta.slice(-1); */
-
-    /* Se acaso a conta tiver um sinal de operação no final, esse IF irá remover para não dar erro
-    if (ultimo_caractere == "+" || ultimo_caractere == "-" || ultimo_caractere == "*" || ultimo_caractere == "/") {
-        visor_conta = visor_conta.substring(0, visor_conta.length - 1);
+    if (numero == "") {
+        visor_conta = document.getElementById('visor_conta').innerHTML = visor_conta.substring(0, visor_conta.length - 1);
         document.getElementById('visor_conta').innerHTML = visor_conta + operacao;
-    } */
+    } else {
+        document.getElementById('visor_conta').innerHTML = eval(visor_conta + numero) + operacao;
+        document.getElementById('visor').innerHTML = "";
+    }
 }
 
 
